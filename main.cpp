694850af4a0071764a9493293f50fc8778098a37
Tezkerek <andrei.ancuta@gmail.com>
2022-02-17 10:47:36 +0200

    Solve perfect numbers problem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!\n";
+    uint a, b;
+    std::cin >> a >> b;
+
+    for (uint x = a; x <= b; ++x) {
+        uint divisor_sum = 0;
+
+        for (uint d = 1; d <= x / 2; ++d) {
+            if (x % d == 0) {
+                divisor_sum += d;
+            }
+        }
+
+        if (divisor_sum == x) {
+            std::cout << x << " ";
+        }
+    }
+
+    std::cout << "sunt numere perfecte.\n";
+
     return 0;
 }
